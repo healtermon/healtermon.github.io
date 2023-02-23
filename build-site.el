b4ff5df7f292bd640e45e034d49aaa963b006d3f
healtermon <healtermon@gmail.com>
2023-02-23 18:45:46 +0800

    added github-actions stuff

--- build-site.el
+++ build-site.el
@@ -1,6 +1,18 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
-(leaf htmlize
-	:straight t)
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
 
 ;; Load the publishing system
 (require 'ox-publish)
